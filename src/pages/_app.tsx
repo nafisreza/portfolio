3930d2ed13a433f3f44026ace26ea1007272b79e
nafisreza <bigganinafis@gmail.com>
2024-11-23 03:31:51 +0600

    Revert "feat: vercel speed insights"
    This reverts commit 7fd67a1c2dc45403e853b2e63b3376979c325b25.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,11 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from '../context/theme-context';
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
       <Component {...pageProps} />
-      <SpeedInsights/>
     </ThemeProvider>
   );
 }
